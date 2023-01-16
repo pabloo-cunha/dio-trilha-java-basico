b1bedc92828a194548accfbc7b344599a32face3
pabloo-cunha <pablo.limacunha@outlook.com>
2023-01-15 22:39:26 -0300

    Exercicios das aulas de Loops

---
+++ estruturas-repeticao-array/src/edu/Pablo/exercicios/loops/Notas.java
@@ -0,0 +1,13 @@
+package edu.Pablo.exercicios.loops;
+
+//Faça um programa que peça uma nota, entre zero e dez.
+//Mostre uma mensagem caso o valor seja inválido
+//e continue pedindo
+//até o usuario informe um valor valido
+
+public class Notas {
+    public static void main(String[] args) {
+
+
+    }
+}
