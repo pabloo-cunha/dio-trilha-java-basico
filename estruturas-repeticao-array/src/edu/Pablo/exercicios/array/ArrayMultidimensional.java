4ad73a8168be1919800b02250c5b909fccef8c34
pabloo-cunha <pablo.limacunha@outlook.com>
2023-01-18 07:47:49 -0300

    Exercicios utilizando Arrays

---
+++ estruturas-repeticao-array/src/edu/Pablo/exercicios/array/ArrayMultidimensional.java
@@ -0,0 +1,27 @@
+package edu.Pablo.exercicios.array;
+/*
+Gere e imprima uma matriz M 4x4 com valores aleatorios entre 0-9.
+ */
+import java.util.Random;
+
+public class ArrayMultidimensional {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[][] M = new int[4][4];
+
+        for (int i = 0; i < M.length;i++){
+            for (int j = 0; j < M[i].length; j++){
+                M[i][j] = random.nextInt(9);
+            }
+        }
+
+        for (int[] linha: M) {
+            for (int coluna: linha) {
+                System.out.println(coluna);
+            }
+
+        }
+    }
+}
