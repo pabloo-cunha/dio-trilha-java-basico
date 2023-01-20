ee58dcc19143b9960c959e84aa9d55ca7b7948d6
pabloo-cunha <pablo.limacunha@outlook.com>
2023-01-20 16:42:20 -0300

    Sobrecarga

--- Metodos/src/Sobrecarga/Main.java
+++ Metodos/src/Sobrecarga/Main.java
@@ -6,9 +6,20 @@ public class Main {
         //quadrilateros
 
         System.out.println("Exercicio Quadrilateros");
-        Quadrilatero.area(3);
-        Quadrilatero.area(5,2);
-        Quadrilatero.area(7,3,9);
-        Quadrilatero.area(3f,2f);
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Area do Quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5,2);
+        System.out.println("Area do Retangulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7,3,9);
+        System.out.println("Area do Trapezio: " + areaTrapezio);
+
+        float areaLosango = Quadrilatero.area(3f,2f);
+        System.out.println("Area do Losango: " + areaLosango);
+
     }
+
+
 }
--- Metodos/src/Sobrecarga/Quadrilatero.java
+++ Metodos/src/Sobrecarga/Quadrilatero.java
@@ -1,22 +1,23 @@
 package Sobrecarga;
 public class Quadrilatero {
 
-    public static void area(double lado){
-        System.out.println("Area do quadrado: " + lado*lado);
+    public static double area(double lado){
+
+        return lado*lado;
     }
 
-    public static void area(double lado1, double lado2){
+    public static double area(double lado1, double lado2){
 
-        System.out.println("Area do Retangulo: " + lado1*lado2);
+       return lado1*lado2;
     }
 
-    public static void area(double baseMaior, double baseMenor, double altura){
+    public static double area(double baseMaior, double baseMenor, double altura){
 
-        System.out.println("Area do trapezio: " + ((baseMaior+baseMenor)*altura));
+        return (baseMaior+baseMenor)*altura;
     }
 
-    public static void area(float diagonal1, float diagonal2){
+    public static float area(float diagonal1, float diagonal2){
 
-        System.out.println("Area do losango: " + (diagonal1 * diagonal2)/2);
+        return (diagonal1 * diagonal2)/2;
     }
 }
