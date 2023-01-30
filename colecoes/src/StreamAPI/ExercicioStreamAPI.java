ac5a83724cfc05d676f1f463884da75e36d6f483
pabloo-cunha <pablo.limacunha@outlook.com>
2023-01-29 23:38:06 -0300

    List, Set, Maps, StreamAPI (Aulas e exercicios)

--- colecoes/src/StreamAPI/ExercicioStreamAPI.java
+++ colecoes/src/StreamAPI/ExercicioStreamAPI.java
@@ -12,7 +12,7 @@ public class ExercicioStreamAPI {
     public static void main(String[] args) {
 
         List<String> numerosAleatorios =
-                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5", "0");
+                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
 
 //        System.out.println("Imprima todos os elementos dessa lista de String: ");
 //        numerosAleatorios.forEach(System.out::println);
